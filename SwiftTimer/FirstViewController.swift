e015d6dc965e3016933f82bb7df0b54989ea8971
RogerFrigola <roger@rogerfrigola.com>
2017-12-18 12:52:12 +0100

    Commit after first weekend.

--- SwiftTimer/FirstViewController.swift
+++ SwiftTimer/FirstViewController.swift
@@ -9,6 +9,15 @@
 import UIKit
 
 class FirstViewController: UIViewController {
+    
+    //MARK: Properties
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var startStopButton: UIButton!
+    
+    var counter = 0.0
+    var timer = Timer()
+    var isPlaying = false
+    var hasPlayed = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +29,39 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    //MARK: Actions
+    @IBAction func startStopTimer2(_ sender: Any) {
+        if(isPlaying) {
+            // STOP
+            timer.invalidate()
+            isPlaying = false
+            hasPlayed = true
+            startStopButton.setTitle("RESET",for: .normal)
+        } else {
+            if(hasPlayed) {
+                //RESET
+                counter = 0.0
+                UpdateTimer()
+                hasPlayed = false
+                startStopButton.setTitle("START",for: .normal)
+            } else {
+                // START
+                timer = Timer.scheduledTimer(timeInterval: 0.07, target: self, selector: #selector(UpdateTimer), userInfo: nil, repeats: true)
+                isPlaying = true
+                startStopButton.setTitle("STOP",for: .normal)
+            }
+        }
+        return
+    }
+    
+    @objc
+    func UpdateTimer() {
+        let s: Double = counter.truncatingRemainder(dividingBy: 60)
+        let m: Int = Int(counter) / 60
+        
+        timeLabel.text = String(format: "%02d:%05.2f", m, s)
+        //timeLabel.text = String(format: "%2.2f", counter)
+        counter = counter + 0.07
+    }
 }
 
--- SwiftTimer/SecondViewController.swift
+++ SwiftTimer/SecondViewController.swift
@@ -7,12 +7,53 @@
 //
 
 import UIKit
+import CoreMotion
+import CoreLocation
 
 class SecondViewController: UIViewController {
+    
+    //MARK: Properties
+    @IBOutlet weak var accelX: UILabel!
+    @IBOutlet weak var accelY: UILabel!
+    @IBOutlet weak var accelZ: UILabel!
+    @IBOutlet weak var speedGPS: UILabel!
+    
+    var motionManager: CMMotionManager!
+    var locationManager: CLLocationManager!
+    var maxXAccel = 0.0
+    var maxYAccel = 0.0
+    var maxZAccel = 0.0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Accelerometer data
+        motionManager = CMMotionManager()
+        motionManager.startAccelerometerUpdates()
+        motionManager.startAccelerometerUpdates(to: OperationQueue.current!, withHandler: { data, error in
+            guard error == nil else { return }
+            guard let accelerometerData = data else { return }
+            let currentAccelX = accelerometerData.acceleration.x
+            let currentAccelY = accelerometerData.acceleration.y
+            let currentAccelZ = accelerometerData.acceleration.z
+            if(abs(currentAccelX) > self.maxXAccel) {
+                self.maxXAccel = abs(currentAccelX)
+            }
+            if(abs(currentAccelY) > self.maxYAccel) {
+                self.maxYAccel = abs(currentAccelY)
+            }
+            if(abs(currentAccelZ) > self.maxZAccel) {
+                self.maxZAccel = abs(currentAccelZ)
+            }
+            self.accelZ.text = "Max acceleration: " + String(format: "%04.2f", self.maxZAccel) + " g"
+            self.accelX.text = "Max turning: " + String(format: "%04.2f", self.maxXAccel) + " g"
+            self.accelY.text = "Max vertical: " + String(format: "%04.2f", self.maxYAccel) + " g"
+        })
+        
+        // GPS data
+        locationManager = CLLocationManager()
+        let speed = locationManager.location!.speed
+        self.speedGPS.text = String(format: "%04.2f", speed)
     }
 
     override func didReceiveMemoryWarning() {
